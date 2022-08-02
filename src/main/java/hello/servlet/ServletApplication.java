b6864ab67c3ba950a892edfa0043d4aa0021ad25
juntae ha <hjt1031@naver.com>
2022-08-02 21:16:57 +0900

    HTTP API, SSR, CSR

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -18,4 +18,17 @@ public class ServletApplication {
  - 개발자는 Response 객체에 HTTP 응답 정보를 편리하게 입력
  - WAS는 Response 객체에 담겨있는 내용으로 HTTP 응답 정보를 생성
 
+HTTP API
+- HTML 이 아니라 데이터를 전달
+- 주로 JSON 형식 사용
+- 다양한 시스템에서 호출
+
+SSR - 서버 사이드 랜더링
+ - 주로 정적인 화면에 사용, 
+ - JSP, 타임리프 -> 백엔드 개발자
+
+CSR - 클라이언트 사이드 렌더링
+ - HTML 결과를 자바스크립트를 사용해 웹브라우저에서 동적으로 생성해서 적용
+ - 예) 구글 지도, Gmail, 구글 캘린더더
+ - React, Vue.js -> 프론트 엔드 개발자 
  */
